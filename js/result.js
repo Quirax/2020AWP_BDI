8968a5b914a094325916293711f439534bfdbe76
unknown <quiraxical@gmail.com>
2020-11-16 20:12:31 +0900

    Applied animation to result.html

---
+++ js/result.js
@@ -0,0 +1,22 @@
+onLoad(function() {
+    $(".slide:not(:first)").add("#title *").css({
+        opacity: 0
+    });
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+        $(".slide:not(:first):not(.animated)").each(function() {
+            var $this = $(this);
+            var top = $this.position().top;
+            var height = $this.height();
+            if(top - (height * 2/3) <= scrollTop)
+                $this.animate({
+                    opacity: 1
+                }, 1000, function() {
+                    $this.addClass("animated");
+                });
+        });
+    }).scroll();
+    $("#title *").animate({
+        opacity: 1
+    }, 1000);
+});
