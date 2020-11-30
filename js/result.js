d6573e59656ad7909b633d262d8505ba8239cb59
unknown <quiraxical@gmail.com>
2020-11-30 19:45:27 +0900

    Show result as text and color

--- js/result.js
+++ js/result.js
@@ -16,6 +16,19 @@ onLoad(function() {
                 });
         });
     }).scroll();
+    $.getJSON("data/test.json", function(data) {
+        var i = parseInt(location.hash.substring(1));
+        var conf = data.results[i];
+        $("#title h1").html(conf.title);
+        $("#title p").html(conf.description);
+        $("body").css({
+            "background-color": conf.background || "white",
+            "color": conf.color || "black"
+        });
+        $("#help > *").css({
+            "border-color": conf.color || "black"
+        });
+    });
     $("#title *").animate({
         opacity: 1
     }, 1000);
